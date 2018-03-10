8a95814a501d543b666148aee9e87660f3e6fd77
Sam Trent <samtrent@Sams-MacBook-Pro.local>
2018-03-10 14:21:16 -0700

    first

--- engine.js
+++ engine.js
@@ -1,16 +1,50 @@
 
 
-function test(request, responce)
+function calculateAmountOwedByWeight()
 {
+  var userSelectionFromForm = request.query.userSelection
+  var userEnterWeight = request.query.weight
+  var price = 0;
 
-  var JSONdata = {data: 'hi'};
-  responce.render('results', JSONdata); // display the results page and pass data to it
+  if(userSelectionFromForm == "Letters(Stamped)")
+  {
+    if(userEnterWeight < 2)
+    {
+      price = 0.50;
+    }
+    else if(userEnterWeight < 3)
+    {
+      price = 0.71;
+    }
+    else if(userEnterWeight < 3.5)
+    {
+      price = 0.92;
+    }
+    else
+    {
+      price = 1.13;
+    }
+  }
+  else if(userSelectionFromForm == "Letters(Metered)")
+  {
+
+  }
+  else if(userSelectionFromForm == "LargeEnvelopes(Flats)")
+  {
+
+  }
+  else if(userSelectionFromForm == "First-ClassPackageService—Retail")
+  {
+
+  }
+
+  return JSONdata = {selection: userSelectionFromForm}, weight: userEnterWeight, price: price;
 }
 
 function sendData(request, responce)
 {
   var userSelectionFromForm = request.query.userSelection
-  var JSONdata = {data: userSelectionFromForm};
+  var JSONdata = calculateAmountOwedByWeight();
   responce.render('results', JSONdata); // display the results page and pass data to it
 }
 
