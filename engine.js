a2d7d6261cba7c282b53d4dcfcc962628147ce6c
Sam Trent <samtrent@Sams-MacBook-Pro.local>
2018-03-10 13:48:43 -0700

    first

--- engine.js
+++ engine.js
@@ -7,6 +7,13 @@ function test(request, responce)
   responce.render('results', JSONdata); // display the results page and pass data to it
 }
 
+function test(request, responce)
+{
+  var userSelectionFromForm = requst.query.userSelection
+  var JSONdata = {data: userSelectionFromForm};
+  responce.render('results', JSONdata); // display the results page and pass data to it
+}
+
 
 //export the function to our server
 module.exports = {test :test};
